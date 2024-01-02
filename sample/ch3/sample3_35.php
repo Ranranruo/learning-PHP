d3cc315a7c4dbe24ce3362ee6bc621635de07b9c
MaTemPong <tlsalstjr58@gmail.com>
2024-01-02 17:38:07 +0900

    a

---
+++ sample/ch3/sample3_35.php
@@ -0,0 +1,18 @@
+<?php
+  $reg = "/^[_\.0-9-a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i";
+  $email = "tlsasltjr58@gmail.com";
+  $check_email = preg_match($reg, $email);
+  if($check_email == true)
+    echo "올바른 이메일 형식입니다.<br/>";
+  else
+    echo "잘못된 이메일 형십입니다.<br/>";
+
+  // php 5.21부터 작동한다
+  $email = "tlsasltjr58@gmail.com";
+  $check_email = filter_var($email, FILTER_VALIDATE_EMAIL);
+  if($check_email == true)
+    echo "올바른 이메일 형식입니다.<br/>";
+  else
+    echo "잘못된 이메일 형십입니다.<br/>";
+    
+?>
